2948a9f660424769e1e7e822ba4206377fa1abb4
Roberto Oliveira <robertoliveirafaria@gmail.com>
2020-11-19 22:17:51 -0300

    prototipo de response para arvore

--- app/models/validaFormula.py
+++ app/models/validaFormula.py
@@ -1,10 +1,19 @@
 
 from pydantic import BaseModel
+from typing import List
 
+class isValidRequest(BaseModel):
+    formula: str
+    
 class isValidResponse(BaseModel):
     error: str
     formula : str
     resultado : bool
 
-class isValidRequest(BaseModel):
-    formula: str
+class tree(BaseModel):
+    name: str
+    size: List[int]
+    children: List
+
+class criaArvoreResponse(BaseModel):
+    treeData : tree
