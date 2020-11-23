ff7d372ca8be250a3f050aff969ec712b2e821eb
Roberto Oliveira <robertoliveirafaria@gmail.com>
2020-11-23 11:22:49 -0300

    Update controller.py

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -9,17 +9,22 @@ def validaFormula(request: isValidRequest):
     arvoreBase = getNo()
     response = isValidResponse(resultado = False, formula = request.formula, error = "", arvore = arvoreBase)
     valida = validaCaracteresFormula(request.formula)
-    parsedFormula = parseFormula(request.formula)
+    # formula = removeEspaco(request.formula)
+    
     if not valida["resultado"]:
         response.error = valida["error"]
         return response
-    elif contSimbolos(request.formula) > 10:
+    
+    if contSimbolos(request.formula) > 10:
         response.error = "A fórmula possui mais de 10 Símbolos Proposicionais."
         return response
-    elif validaNotNot(request.formula):
+    
+    if validaNotNot(request.formula):
         response.error = "A fórmula tem mais de um símbolo de negação em sequência (parentização incompleta)."
         return response
-    elif not validaParenteseCompleto(parsedFormula):
+
+    parsedFormula = parseFormula(request.formula)
+    if not validaParenteseCompleto(parsedFormula):
         response.error = "Parentização não está completa"
         return response
 
@@ -28,5 +33,3 @@ def validaFormula(request: isValidRequest):
     response.error = res["error"]
     response.arvore = res["arvore"]
     return response
-
-
